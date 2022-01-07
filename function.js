f181c2409a25e138aff4788eb39deeb962ebfe12
BAKHONGMIN <ghdals2655@naver.com>
2022-01-07 18:13:22 +0900

    구구단 계산기

--- function.js
+++ function.js
@@ -18,13 +18,12 @@ var sum = 0;
 var inmyScore = '';
 window.onload = function(){
     for(let i=0 ; i<score.length; i++){
-        inmyScore += `<p>${score[i][0]} ${score[i][1]}</p>`};
-    for(let a=0 ; a<score.length; a++){
+        inmyScore += `<p>${score[i][0]} ${score[i][1]}</p>`;
         sum += score[a][1]};
         fun(`main`, inmyScore);
         fun(`add`, `합계 ${sum}`)};
 
-
+ 
 
 
 
---
+++ gugu.js
@@ -0,0 +1,18 @@
+window.onload = ()=>{
+    document.getElementById("trigger").onclick=()=>{
+        let inputnum = document.getElementById('guguNum').value;
+        guguDan(inputnum);
+    }
+
+// window.onload = function(){}와 같다
+// 3단이 출력되도록 처리하기
+function guguDan(num){   
+    let guguText="";    
+    for(let i=0; i<10; i++){
+        guguText += `<li>${num}X${i}=${num*i}</li>`;
+        }
+    document.getElementById("gugu").innerHTML = guguText;
+    }
+
+}
+
